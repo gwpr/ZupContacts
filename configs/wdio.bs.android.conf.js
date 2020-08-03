5533b0aa46f9ab42f9fdebf3ba4025745c4eaee3
Gilvan Wemerson <gilvanwemerson@MacBook-Pro-de-Gilvan.local>
2020-08-03 02:24:21 -0300

    FINALIZANDO

--- configs/wdio.bs.android.conf.js
+++ configs/wdio.bs.android.conf.js
@@ -14,8 +14,8 @@ exports.config = {
     browserstackLocal: true,
     
     specs: [
-        // './test/contacts/*',
-        './test/contacts/listContactByNameTest.js',
+        './test/**/*',
+        // './test/contacts/listContactByNameTest.js',
     ],
     exclude:
     [],
